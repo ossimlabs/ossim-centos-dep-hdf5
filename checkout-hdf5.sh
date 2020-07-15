da2d3d9126514aed645e8406b59e349790592490
randomtree07 <ryanfeldbush@gmail.com>
2020-07-15 10:57:49 -0400

    Added hdf5 version

--- checkout-hdf5.sh
+++ checkout-hdf5.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-HDF5="hdf5-${HDF5_VERSION}"
-dir="${DEPS_DIR}/hdf5"
+HDF5="hdf5-1.10.5"
+
 
 if [ ! -d "${dir}" ]; then
     wget -q https://s3.amazonaws.com/ossimlabs/dependencies/source/$HDF5.tgz -O $HDF5.tgz
     tar xvfz $HDF5.tgz
     rm -f $HDF5.tgz
-    mv "${HDF5}" "${DEPS_DIR}/hdf5"
+    mv "${HDF5}" "hdf5"
 fi
